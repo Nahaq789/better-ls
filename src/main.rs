380e495f49d92c3bde8b5bd1a27f8948d53badca
Nahaq789 <naha022677@icloud.com>
2025-03-10 17:41:09 +0900

    feat: key map

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,33 @@
-use clap::Parser;
-use structs::Cli;
+use std::env;
 
 pub mod structs;
 
-fn main() -> anyhow::Result<()> {
-    // let args = Cli::parse();
-    // println!("{:?}, {:?}", args.path(), args.pattern());
-    // let current = std::fs::read_to_string(args.path()).expect("could not read file");
-    // println!("{:?}", current);
-    //
-    // for line in current.lines() {
-    //     if line.contains(args.pattern()) {
-    //         println!("{:?}", line)
-    //     }
-    // } 
+const KEY_SET: &'static [&'static str] = &[
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+    "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P",
+    "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+];
 
-    // let mut command = std::process::Command::new("ls");
-    // command
-    //     .spawn()
-    //     .expect("ls command failed to start");
-    //
-    // let output = match command.output() {
-    //     Ok(o) => o,
-    //     Err(_) => panic!("error")
-    // };
-    // println!("{:?}", output)
-    //
-    let paths = std::fs::read_dir("./")?;
-    for path in paths {
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let path = &args[1];
+    let paths = std::fs::read_dir(path)?;
+    for (i, path) in paths.into_iter().enumerate() {
         match path {
             Ok(it) => {
                 let folder = &it.file_name();
                 let file_type = it.file_type()?;
-                
+
                 if file_type.is_file() {
                     continue;
                 }
-                println!("{}, {:?}", folder.to_str().unwrap(), file_type);
-            },
+                println!("key: {}  {}", KEY_SET[i], folder.to_str().unwrap());
+            }
             Err(e) => {
                 println!("{:?}", e)
             }
-        } 
+        }
     }
     Ok(())
 }
